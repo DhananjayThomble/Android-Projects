8439626bc6af870d1c9d34c551e2f341d19cdd28
Dhananjay Thomble <41919659+DhananjayThomble@users.noreply.github.com>
2020-10-01 18:06:34 +0530

    auto-populate countries name
    Replacing edit text with spinner. Also added code for auto populating countries name.

--- Covidtracker/Code/app/src/main/java/com/prefycs/covidtracker/MainActivity.java
+++ Covidtracker/Code/app/src/main/java/com/prefycs/covidtracker/MainActivity.java
@@ -1,17 +1,16 @@
 package com.prefycs.covidtracker;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-
 import android.view.View;
-import android.widget.Adapter;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -22,29 +21,43 @@ import com.android.volley.toolbox.Volley;
 
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText search;
+    Spinner search;
     ImageView img;
     TextView country;
     ListView lst;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        search=findViewById(R.id.search);
-        lst=findViewById(R.id.lstv);
-        country=findViewById(R.id.country);
+        search = findViewById(R.id.search);
+        lst = findViewById(R.id.lstv);
+        country = findViewById(R.id.country);
+
+        //This will populate country name.
+        popCountry();
 
 
+        img = findViewById(R.id.ico);
+        search.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                LoadApi();
+            }
 
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
 
-        img=findViewById(R.id.ico);
-
-
+            }
+        });
         img.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -55,13 +68,31 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    public void popCountry() {
+        Locale[] locales = Locale.getAvailableLocales();
+        ArrayList<String> countries = new ArrayList<String>();
+        for (Locale locale : locales) {
+            String country = locale.getDisplayCountry();
+            if (country.trim().length() > 0 && !countries.contains(country)) {
+                countries.add(country);
+            }
+        }
 
-    public  void LoadApi()
-    {
+        Collections.sort(countries);
+//
+
+        ArrayAdapter<String> countryAdapter = new ArrayAdapter<>(getApplicationContext(),
+                android.R.layout.simple_spinner_item, countries);
+
+        countryAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the your spinner
+        search.setAdapter(countryAdapter);
+    }
+
+    public void LoadApi() {
 
 
-
-        String query = search.getText().toString();
+        String query = search.getSelectedItem().toString();
 
         RequestQueue queue = Volley.newRequestQueue(this);
 
@@ -79,14 +110,14 @@ public class MainActivity extends AppCompatActivity {
 
 
                             String cs = response.getString("cases");
-                            String tdc=response.getString("todayCases");
+                            String tdc = response.getString("todayCases");
                             String dt = response.getString("deaths");
-                            String tdd=response.getString("todayDeaths");
-                            String acc=response.getString("active");
+                            String tdd = response.getString("todayDeaths");
+                            String acc = response.getString("active");
                             String rec = response.getString("recovered");
 
-                            String[] array = {"Cases: "+cs,"Today Cases: "+tdc,"Active: " +acc,"Deaths: " +dt,"today deaths: "+tdd,"Recovered: " +rec};
-                            ArrayAdapter adapter = new ArrayAdapter<String>(MainActivity.this,R.layout.activity_listview,array);
+                            String[] array = {"Cases: " + cs, "Today Cases: " + tdc, "Active: " + acc, "Deaths: " + dt, "today deaths: " + tdd, "Recovered: " + rec};
+                            ArrayAdapter adapter = new ArrayAdapter<String>(MainActivity.this, R.layout.activity_listview, array);
                             lst.setAdapter(adapter);
 
                         } catch (JSONException e) {
@@ -98,7 +129,7 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onErrorResponse(VolleyError error) {
                         // TODO: Handle error
-                        Toast.makeText(getApplicationContext(), "Connection Failed",Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getApplicationContext(), "Connection Failed", Toast.LENGTH_SHORT).show();
 
 
                     }
